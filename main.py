43ef0ff93a158641eb7658016cf6ee92e0db330e
Akashjatav98 <78926809+Akashjatav98@users.noreply.github.com>
2021-06-08 19:16:33 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+
+# Python 🐍 3.7.1 
+print ("")
+
+print ("")
+print ("")
+print ("")
+print ("")
+
+
+print ("")
+
+print ("")
+print("''''''''''''''''''''''''''''''''''''''''''")
+print("''  🇮🇳 THE DEVELOPER BY AKASH JATAV  🇮🇳 ''")
+print("''''''''''''''''''''''''''''''''''''''''''")
+
+print ("")
+print ("__________________________________________")
+print ("")
+print ("//////////////////////////////////////////")
+print ("//        ENTER THE TABLE NUMBER        //")
+print ("//////////////////////////////////////////")
+print ("")
+print ("")
+name =input("ENTER THE NAME :  ")
+print ("")
+
+
+
+n=  int(input("PLEASE  ENTER THE NUMBER :  "))
+
+print ("")
+print ('⬇️⬇️ HELLo',name,'YOUR TABLE ⬇️⬇️')     
+print ("")
+  
+print(n*1)
+print(n*2)
+print(n*3)
+print(n*4)
+print(n*5)
+print(n*6)
+print(n*7)
+print(n*8) 
+print(n*9)
+print(n*10)
+
+  
+    
+i=1
+n=int(input("Enter a number"))
+while i<=10:
+  a=i*n
+  print(n,"*",i,"=",a)
+  i+=1
+  
+ 
+  
+ 
